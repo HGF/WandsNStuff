a1c6c717833c1541b736a9630839ef7b7cfc5a75
HGF <bradherrmann1@gmaill.com>
2012-01-29 23:58:07 -0600

    Added in the player listener

--- src/main/java/edu/uic/cs/acm/WandsNStuff/WandsNStuff.java
+++ src/main/java/edu/uic/cs/acm/WandsNStuff/WandsNStuff.java
@@ -1,9 +1,10 @@
-package edu.uic.cs.acm.WandsNStuff;
+package main.java.edu.uic.cs.acm.WandsNStuff;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class WandsNStuff extends JavaPlugin{
 
+    private final WandPlayerListener playerListener = new WandPlayerListener(this);
     public void onDisable() {
 
     }
